845bab7aee5213fdc3530c90a3c13b3b825d8d74
Hardy Smith <hardysmith940110@gmail.com>
2024-09-15 05:40:10 +1000

    feat: convert project to teable-platform init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use actix_web::{
-    App,
-    HttpServer, 
-    web::Data
-};
+use actix_web::{ App, HttpServer, web::Data };
 
 mod api;
 mod model;
@@ -12,24 +8,21 @@ mod prelude;
 mod error;
 
 use repository::surrealdb_repo::SurrealDBRepo;
-use api::todo_api::{create_todo, get_todos, get_todo, update_todo, delete_todo};
-
+use api::todo_api::{ create_todo };
+//, get_todos, get_todo, update_todo, delete_todo
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let surreal = SurrealDBRepo::init().await.expect("Error connecting to SurrealDB!");
-    
+
     let db_data = Data::new(surreal);
-    
-    HttpServer::new(move || { 
-        App::new()
-            .app_data(db_data.clone())
-            .service(create_todo)
-            .service(get_todos)
-            .service(get_todo)
-            .service(update_todo)
-            .service(delete_todo)
-        })
+
+    HttpServer::new(move || {
+        App::new().app_data(db_data.clone()).service(create_todo)
+        // .service(get_todos)
+        // .service(get_todo)
+        // .service(update_todo)
+        // .service(delete_todo)
+    })
         .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+        .run().await
 }
--- src/model/todo_model.rs
+++ src/model/todo_model.rs
@@ -1,58 +1,84 @@
 use actix_web::web::Data;
 use std::collections::BTreeMap;
-use serde::{Serialize, Deserialize};
-use surrealdb::sql::{Object, Value, thing, Array};
+use serde::{ Serialize, Deserialize };
+use surrealdb::sql::{ thing, Array, Number, Object, Value };
 
 use crate::prelude::*;
-use crate::utils::{macros::map};
-use crate::repository::surrealdb_repo::{Creatable, Patchable, SurrealDBRepo};
+use crate::utils::{ macros::map };
+use crate::repository::surrealdb_repo::{ Creatable, Patchable, SurrealDBRepo };
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Content {
+    pub content_type: String,
+    pub content_body: String,
+}
 
+impl From<Content> for Value {
+    fn from(content: Content) -> Self {
+        map![
+            "content_type".into() => content.content_type.into(),
+            "content_body".into() => content.content_body.into()
+        ].into()
+    }
+}
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Todo {
     pub id: Option<String>,
     pub title: String,
-    pub body: String,
+    pub body: Vec<Vec<Content>>,
 }
 
 impl From<Todo> for Value {
     fn from(val: Todo) -> Self {
+        let body_values: Vec<Value> = val.body
+            .into_iter()
+            .map(|inner_vec| {
+                let inner_values: Vec<Value> = inner_vec.into_iter().map(Value::from).collect();
+                Value::Array(inner_values.into())
+            })
+            .collect();
         match val.id {
             Some(v) => {
                 map![
                     "id".into() => v.into(),
                     "title".into() => val.title.into(),
-                    "body".into() => val.body.into(),
-            ].into()
-            },
+                    "body".into() => body_values.into(),
+                ].into()
+            }
             None => {
                 map![
                     "title".into() => val.title.into(),
-                    "body".into() => val.body.into()
+                    "body".into() => body_values.into()
                 ].into()
             }
         }
     }
 }
 
-impl Creatable for Todo{}
+impl Creatable for Todo {}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TodoPatch {
     pub title: Option<String>,
-    pub body: Option<String>,
+    pub body: Option<Vec<Vec<Content>>>,
 }
 
 impl From<TodoPatch> for Value {
     fn from(val: TodoPatch) -> Self {
-
         let mut value: BTreeMap<String, Value> = BTreeMap::new();
-        
+
         if let Some(t) = val.title {
             value.insert("title".into(), t.into());
         }
 
         if let Some(b) = val.body {
-            value.insert("body".into(), b.into());
+            let body_values: Vec<Value> = b
+                .into_iter()
+                .map(|inner_vec| {
+                    let inner_values: Vec<Value> = inner_vec.into_iter().map(Value::from).collect();
+                    Value::Array(inner_values.into())
+                })
+                .collect();
+            value.insert("body".into(), Value::Array(body_values.into()));
         }
         Value::from(value)
     }
@@ -60,85 +86,100 @@ impl From<TodoPatch> for Value {
 
 impl Patchable for TodoPatch {}
 
-
 pub struct TodoBMC;
 
 impl TodoBMC {
-
     pub async fn get_all(db: Data<SurrealDBRepo>) -> Result<Vec<Object>, Error> {
         let ast = "SELECT * FROM todo;";
 
         let res = db.ds.execute(ast, &db.ses, None, true).await?;
-        
+
         let first_res = res.into_iter().next().expect("Did not get a response");
 
         let array: Array = W(first_res.result?).try_into()?;
 
-        array.into_iter().map(|value| W(value).try_into()).collect()
+        array
+            .into_iter()
+            .map(|value| W(value).try_into())
+            .collect()
     }
 
-    pub async fn create<T: Creatable>(db: Data<SurrealDBRepo>, tb: &str, data: T) -> Result<Object, Error> {
+    pub async fn create<T: Creatable>(
+        db: Data<SurrealDBRepo>,
+        tb: &str,
+        data: T
+    ) -> Result<Object, Error> {
         let sql = "CREATE type::table($tb) CONTENT $data RETURN *";
 
         let data: Object = W(data.into()).try_into()?;
 
-		let vars: BTreeMap<String, Value> = map![
+        let vars: BTreeMap<
+            String,
+            Value
+        > = map![
 			"tb".into() => tb.into(),
 			"data".into() => Value::from(data)];
 
-		let ress = db.ds.execute(sql, &db.ses, Some(vars), false).await?;
-		
-        let first_val = ress.into_iter().next().map(|r| r.result).expect("id not returned")?;
-        
+        let ress = db.ds.execute(sql, &db.ses, Some(vars), false).await?;
+
+        let first_val = ress
+            .into_iter()
+            .next()
+            .map(|r| r.result)
+            .expect("id not returned")?;
+
         W(first_val.first()).try_into()
     }
 
     pub async fn get(db: Data<SurrealDBRepo>, tid: &str) -> Result<Object, Error> {
         let sql = "SELECT * FROM $th";
-            
-            let tid = format!("todo:{}", tid);
-
-            let vars: BTreeMap<String, Value> = map!["th".into() => thing(&tid)?.into()];
-    
-            let ress = db.ds.execute(sql, &db.ses, Some(vars), true).await?;
-    
-            let first_res = ress.into_iter().next().expect("Did not get a response");
-    
-            W(first_res.result?.first()).try_into()
-           
-    }
-    
-    pub async fn update<T: Patchable>(db: Data<SurrealDBRepo>, tid: &str, data: T) -> Result<Object, Error> {
-		let sql = "UPDATE $th MERGE $data RETURN *";
 
         let tid = format!("todo:{}", tid);
 
-		let vars = map![
+        let vars: BTreeMap<String, Value> = map!["th".into() => thing(&tid)?.into()];
+
+        let ress = db.ds.execute(sql, &db.ses, Some(vars), true).await?;
+
+        let first_res = ress.into_iter().next().expect("Did not get a response");
+
+        W(first_res.result?.first()).try_into()
+    }
+
+    pub async fn update<T: Patchable>(
+        db: Data<SurrealDBRepo>,
+        tid: &str,
+        data: T
+    ) -> Result<Object, Error> {
+        let sql = "UPDATE $th MERGE $data RETURN *";
+
+        let tid = format!("todo:{}", tid);
+
+        let vars = map![
 			"th".into() => thing(&tid)?.into(),
 			"data".into() => data.into()];
 
-		let ress = db.ds.execute(sql, &db.ses, Some(vars), true).await?;
+        let ress = db.ds.execute(sql, &db.ses, Some(vars), true).await?;
 
-		let first_res = ress.into_iter().next().expect("id not returned");
+        let first_res = ress.into_iter().next().expect("id not returned");
 
-		let result = first_res.result?;
-        
+        let result = first_res.result?;
+
         W(result.first()).try_into()
-	}
+    }
 
     pub async fn delete(db: Data<SurrealDBRepo>, tid: &str) -> Result<String, Error> {
-		let sql = "DELETE $th RETURN *";
+        let sql = "DELETE $th RETURN *";
 
         let tid = format!("todo:{}", tid);
 
-		let vars = map!["th".into() => thing(&tid)?.into()];
+        let vars = map!["th".into() => thing(&tid)?.into()];
 
-		let ress = db.ds.execute(sql, &db.ses, Some(vars), false).await?;
+        let ress = db.ds.execute(sql, &db.ses, Some(vars), false).await?;
 
-		let first_res = ress.into_iter().next().expect("id not returned");
+        let first_res = ress.into_iter().next().expect("id not returned");
 
-		first_res.result?;
+        first_res.result?;
 
         Ok(tid)
-	}
+    }
 }
