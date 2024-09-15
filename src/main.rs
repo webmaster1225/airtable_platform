88595c6a1a252fb8e0bb5e67fe9ff4e1bb15bf6f
Hardy <hardysmith940110@gmail.com>
2024-09-15 13:50:15 +1000

    feat: get_all, delete_id, put_id

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ mod prelude;
 mod error;
 
 use repository::surrealdb_repo::SurrealDBRepo;
-use api::todo_api::{ create_todo };
-//, get_todos, get_todo, update_todo, delete_todo
+use api::todo_api::{ create_todo, get_todos, get_todo, update_todo, delete_todo };
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let surreal = SurrealDBRepo::init().await.expect("Error connecting to SurrealDB!");
@@ -17,11 +17,13 @@ async fn main() -> std::io::Result<()> {
     let db_data = Data::new(surreal);
 
     HttpServer::new(move || {
-        App::new().app_data(db_data.clone()).service(create_todo)
-        // .service(get_todos)
-        // .service(get_todo)
-        // .service(update_todo)
-        // .service(delete_todo)
+        App::new()
+            .app_data(db_data.clone())
+            .service(create_todo)
+            .service(get_todos)
+            .service(get_todo)
+            .service(update_todo)
+            .service(delete_todo)
     })
         .bind(("127.0.0.1", 8080))?
         .run().await
